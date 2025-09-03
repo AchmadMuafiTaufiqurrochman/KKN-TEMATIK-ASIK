d70e9c9e05e8cc016f4296aa543f3b01a1800860
faulnam <syifakul.anm@gmail.com>
2025-09-03 21:31:58 +0700

    LOGO YE

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -7,8 +7,9 @@
     <div class="max-w-md w-full space-y-8">
         <div>
             <div class="flex justify-center">
-                <div class="w-16 h-16 bg-primary rounded-full flex items-center justify-center">
-                    <span class="text-secondary font-bold text-2xl">MS</span>
+                <div class="w-16 h-16 rounded-full flex items-center justify-center">
+                   <img src="{{ asset('img/logo_sidoarjo.png') }}" alt="Logo Sidoarjo"
+                        class="w-full h-full object-contain">
                 </div>
             </div>
             <h2 class="mt-6 text-center text-3xl font-bold text-primary">
